4bc49a2475e97df0fd43486207f987612eb5997e
kylemann16 <kyle@hobu.co>
2023-08-10 15:06:49 -0500

    Reading ept

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
+#include <string>
+#include <exception>
+
 #include <pdal/PipelineManager.hpp>
+#include <pdal/StageFactory.hpp>
+#include <pdal/Reader.hpp>
+#include <pdal/Stage.hpp>
+
+using namespace pdal;
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello, world" << std::endl;
+    std::string eptPath = "https://s3-us-west-2.amazonaws.com/usgs-lidar-public/MD_GoldenBeach_2012/ept.json";
+    StageFactory sf = StageFactory();
+    PipelineManagerPtr pm = std::make_unique<PipelineManager>();
+
+    std::string driver = sf.inferReaderDriver(eptPath);
+    if (driver.empty())
+        throw ("File not recognized");
+
+    Stage *s = &pm->addReader(driver);
+
+    // PointTable table;
+    Options o;
+    o.add("filename", eptPath);
+    s->addOptions(o);
+    // s->prepare(table);
+
+    // Stage *info = &pm->addFilter("filters.info");
+    pm->execute(ExecMode::PreferStream);
+    // std::cout <<"info metadata? " << pdal::Utils::toJSON(info->getMetadata()) << std::endl;
+    std::cout <<"reader metadata? " << pdal::Utils::toJSON(s->getMetadata()) << std::endl;
+
     return 0;
 }
