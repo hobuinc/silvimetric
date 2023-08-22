635efa145f77b429759db6fd4a0a3c5900bee63f
kylemann16 <kyle@hobu.co>
2023-08-22 14:34:47 -0500

    added arguments and stat collection

--- python/main.py
+++ python/main.py
@@ -5,15 +5,19 @@ import numpy as np
 from dataclasses import dataclass
 from dask.distributed import Client, progress
 import time
+import argparse
 
 
 #starting variables
 # filename = "https://s3.amazonaws.com/hobu-lidar/autzen-classified.copc.laz"
-filename = "autzen-classified.copc.laz"
-cell_size = 100
+# filename = "autzen-classified.copc.laz"
+# cell_size = 100
+# workers = 12
+# threads = 4
+# dask_bool = True
 
 class Bounds(object):
-    def __init__(self, minx, miny, maxx, maxy, cell_size, srs=None):
+    def __init__(self, minx, miny, maxx, maxy, cell_size, group_size, srs=None):
         self.minx = float(minx)
         self.miny = float(miny)
         self.maxx = float(maxx)
@@ -58,12 +62,14 @@ class Chunk(object):
         self.x1, self.x2 = xrange
         self.y1 , self.y2 = yrange
         self.parent_bounds = parent
+        cell_size = parent.cell_size
+        group_size = parent.group_size
+        srs = parent.srs
         minx = (self.x1 * cell_size) + parent.minx
         miny = (self.y1 * cell_size) + parent.miny
         maxx = (self.x2 + 1) * cell_size + parent.minx
         maxy = (self.y2 + 1) * cell_size + parent.miny
-        self.bounds = Bounds(minx, miny, maxx, maxy, parent.cell_size,
-                            parent.srs);
+        self.bounds = Bounds(minx, miny, maxx, maxy, cell_size, group_size, srs)
         self.indices = [[i,j] for i in range(self.x1, self.x2+1)
                         for j in range(self.y1, self.y2+1)]
 
@@ -75,48 +81,72 @@ def arrange_data(point_data, bounds):
     # Set up data object
     dx = []
     dy = []
-    z_data = np.empty((0,0))
-    count = np.array((0), np.int64)
+    count = np.array([], dtype=np.int32)
+    z_data = np.zeros((points.size), dtype=np.float64)
 
     xis = np.floor((points['X'] - bounds.minx) / bounds.cell_size)
     yis = np.floor((points['Y'] - bounds.miny) / bounds.cell_size)
 
+
+    offset = 0
+    offsets = []
     for x, y in chunk.indices:
+        add_data = np.array([points["Z"][np.logical_and(
+            xis.astype(np.int32) == x, yis.astype(np.int32) == y)]],
+            np.float64, copy=False)
 
-        if z_data.size == 0:
-            add_data = np.array([points["Z"][np.logical_and(
-                xis.astype(np.int32) == x, yis.astype(np.int32) == y)]],
-                np.float64)
-            if add_data.any():
-                dx.append(x)
-                dy.append(y)
-                count = np.array([add_data.size], np.int64)
-                z_data = add_data
-        else:
-            add_data = np.array([points["Z"][np.logical_and(
-                xis.astype(np.int32) == x, yis.astype(np.int32) == y)]],
-                np.float64)
+        if add_data.any():
+            dx.append(x)
+            dy.append(y)
+            size = add_data.size
 
-            if add_data.any():
-                dx.append(x)
-                dy.append(y)
-                count = np.append(count, add_data.size)
-                cur_length = z_data.shape[1]
-                if add_data.size < cur_length:
-                    add_data = np.pad(add_data,
-                        [(0,0), (0, z_data[0].size - add_data.size)],
-                        mode='constant',
-                        constant_values=float("nan"))
-                elif add_data.size > cur_length:
-                    z_data = np.pad(z_data,
-                        [(0,0), (0, add_data.size - z_data[0].size)],
-                        mode='constant',
-                        constant_values=float("nan"))
-                z_data = np.concatenate((z_data, add_data))
+            count = np.append(count, size)
+
+            n = offset + size
+            z_data[offset:n] = add_data
+            offsets.append([offset, n])
+            offset = n
+            # else:
+            #     z_data[index] = np.delete(z_data[index])
+
+        # else:
+        #     add_data = np.array([points["Z"][np.logical_and(
+        #         xis.astype(np.int32) == x, yis.astype(np.int32) == y)]],
+        #         np.float64, copy=False)
+
+        #     if add_data.any():
+        #         dx.append(x)
+        #         dy.append(y)
+        #         count = np.append(count, add_data.size)
+        #         cur_length = z_data.shape[1]
+        #         if add_data.size < cur_length:
+        #             add_data = np.pad(add_data,
+        #                 [(0,0), (0, z_data[0].size - add_data.size)],
+        #                 mode='constant',
+        #                 constant_values=float("nan"))
+        #         elif add_data.size > cur_length:
+        #             z_data = np.pad(z_data,
+        #                 [(0,0), (0, add_data.size - z_data[0].size)],
+        #                 mode='constant',
+        #                 constant_values=float("nan"))
+        #         z_data = np.concatenate((z_data, add_data))
 
     # add last row so that tiledb doesn't throw us out
     # count = np.array([z.size for z in z_data], np.int64)
-    dd = dict(count=count, Z=z_data)
+
+    # s = time.perf_counter_ns()
+    r = np.array([z_data[lo:hi] for lo,hi in offsets], dtype=object)
+    # e = time.perf_counter_ns()
+    # r2 = np.array(np.array_split(z_data, [hi-lo for lo, hi in offsets]), dtype=object)
+    # e2 = time.perf_counter_ns()
+
+    # print("Time r: ", e - s)
+    # print("Time r2: ", e2 - e)
+    # z_data = np.array(np.array_split(z_data, [r for r in offsets]))
+    if r.size == count.size:
+        dd = dict(count=count, Z=r)
+    else:
+        dd = dict(count=count, Z=np.array([r, None], object)[:-1])
     return [dx, dy, dd]
 
 def get_data(reader, chunk):
@@ -126,7 +156,7 @@ def get_data(reader, chunk):
     reader._options['threads'] = 1
     pipeline = reader.pipeline()
     pipeline.execute()
-    points = np.array(pipeline.arrays[0])
+    points = np.array(pipeline.arrays[0], copy=False)
 
     return [points, chunk]
 
@@ -146,7 +176,7 @@ def create_tiledb(bounds: Bounds):
             capacity=bounds.xi*bounds.yi, attrs=[count_att, z_att])
         tiledb.SparseArray.create('stats', schema)
 
-def create_bounds(pipeline) -> Bounds:
+def create_bounds(pipeline, cell_size, group_size) -> Bounds:
     # grab our bounds
     qi = pipeline.quickinfo['readers.copc']
     bbox = qi['bounds']
@@ -156,7 +186,15 @@ def create_bounds(pipeline) -> Bounds:
     maxy = bbox['maxy']
     srs = qi['srs']['wkt']
 
-    return Bounds(minx, miny, maxx, maxy, cell_size=cell_size, srs=srs)
+    return Bounds(minx, miny, maxx, maxy, cell_size=cell_size,
+                  group_size=group_size, srs=srs)
+
+def write_tdb(tdb, res):
+    dx, dy, dd = res
+    tdb[dx, dy] = dd
+    return dd['count'].sum()
+
+
 
 # tiledb errors if np array size is greater than array's first dim
 # work around this by adding a None value to the array, which makes
@@ -165,16 +203,17 @@ def create_bounds(pipeline) -> Bounds:
 def unpack_ndarrays(da, index_len: int):
     for arr in da:
         if da[arr].size > index_len:
-            da[arr] = np.array([*da[arr], [None]], object)[:-1]
+            da[arr] = np.array([*da[arr], [None]], object, copy=False)[:-1]
     return da
 
-def main():
-    start = time.time()
+def main(filename: str, threads: int, workers: int, group_size: int, res: float,
+         no_threads: bool, stats_bool: bool):
+
 
     # read pointcloud
     reader = pdal.Reader(filename)
     pipeline = reader.pipeline()
-    bounds = create_bounds(pipeline)
+    bounds = create_bounds(pipeline, res, group_size)
 
     # set up tiledb
     create_tiledb(bounds)
@@ -189,24 +228,69 @@ def main():
         if (bounds.srs):
             tdb.meta["CRS"] = bounds.srs
 
-        client = Client(threads_per_worker=12, n_workers=4,
-            serializers=['dask', 'pickle'], deserializers=['dask', 'pickle'])
+        if stats_bool:
+            pc = 0
+            cell_count = bounds.xi * bounds.yi
+            chunk_count = 0
+            start = time.perf_counter_ns()
 
+        if no_threads:
+            for ch in bounds.chunk():
+                point_data = get_data(reader=reader, chunk=ch)
+                arranged_data = arrange_data(point_data=point_data, bounds=bounds)
+                point_count = write_tdb(tdb, arranged_data)
+                if stats_bool:
+                    pc += point_count
+                    chunk_count += 1
 
-        # chunks = bounds.chunk()
-        # chs = client.scatter(chunks)
-        print("Reading and arranging chunks...")
-        point_futures = [client.submit(get_data, reader=reader, chunk=ch) for ch in bounds.chunk()]
-        data_futures = [client.submit(arrange_data, point_data=pf, bounds=bounds) for pf in point_futures]
-        progress(*[data_futures, point_futures])
+        else:
+            client = Client(threads_per_worker=threads, n_workers=workers,
+                serializers=['dask', 'pickle'], deserializers=['dask', 'pickle'])
 
-        for df in data_futures:
-            dx, dy, dd = df.result()
-            if len(dx):
-                data = unpack_ndarrays(dd, len(dx))
-                tdb[dx, dy] = data
-        end = time.time()
-        print("Done. Time elapsed: ", end-start)
+            # chunks = bounds.chunk()
+            # chs = client.scatter(chunks)
+            print("Reading to pdal")
+            point_futures = [client.submit(get_data, reader=reader, chunk=ch) for ch in bounds.chunk()]
+            print("Arranging data")
+            data_futures = [client.submit(arrange_data, point_data=pf, bounds=bounds) for pf in point_futures]
+            print("Writing data")
+            write_futures = [client.submit(write_tdb, tdb=tdb, res=df) for df in data_futures]
+            futures = [ data_futures, point_futures, write_futures ]
+            progress(*futures)
+            client.gather(*futures)
+            if stats_bool:
+                pc = 0
+                chunk_count = len(data_futures)
+                for wf in write_futures:
+                    pc += wf.result()
+
+        if stats_bool:
+            end = time.perf_counter_ns()
+            stats = dict(
+                time=(end-start)/float(1000000000), threads=threads, workers=workers,
+                group_size=group_size, resolution=res, point_count=pc,
+                cell_count=cell_count, chunk_count=chunk_count
+            )
+            print(stats)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", type=str)
+    parser.add_argument("--threads", type=int, default=4)
+    parser.add_argument("--workers", type=int, default=12)
+    parser.add_argument("--group_size", type=int, default=3)
+    parser.add_argument("--resolution", type=float, default=100)
+    parser.add_argument("--no_threads", type=bool, default=False)
+    parser.add_argument("--stats", type=bool, default=False)
+
+    args = parser.parse_args()
+
+    filename = args.filename
+    threads = args.threads
+    workers = args.workers
+    group_size = args.group_size
+    res = args.resolution
+    no_threads = args.no_threads
+    stats_bool = args.stats
+
+    main(filename, threads, workers, group_size, res, no_threads, stats_bool)
