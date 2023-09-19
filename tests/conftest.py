afa3216d653a646de2e522ef44d038e9c2bce869
Ognyan Moore <ognyan.moore@gmail.com>
2023-09-19 12:05:16 -0700

    Got pytest working

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+import os
+import dask
+
+@pytest.fixture(scope="session")
+def autzen_classified() -> str:
+    path = os.path.join(
+            os.path.dirname(__file__),
+            "data",
+            "autzen-classified.copc.laz"
+    )
+    assert os.path.exists(path)
+    return os.path.abspath(path)
+
+@pytest.fixture(scope="session", autouse=True)
+def configure_dask():
+    dask.config.set(scheduler="single-threaded")
