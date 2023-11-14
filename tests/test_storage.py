a905cc4e80e4255cef0453d0287697d838845b42
kylemann16 <kyle@hobu.co>
2023-11-14 12:25:30 -0600

    Comment out storage tests until they're ready to go

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -3,9 +3,9 @@ import tiledb
 
 from silvistat.storage import Storage
 
-class Test_Storage(object):
-    @pytest.fixture(scope="class")
-    def schema() -> tiledb.ArraySchema:
+# class Test_Storage(object):
+#     @pytest.fixture(scope="class")
+#     def schema() -> tiledb.ArraySchema:
 
 
-    def test_schema()
+#     def test_schema()
