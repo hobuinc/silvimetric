a32a7e14a07ac4ccf56a823772a5423fbbfaa1d5
kylemann16 <kyle@hobu.co>
2023-11-15 17:18:28 -0600

    Made storage class more robust

--- src/silvistat/storage.py
+++ src/silvistat/storage.py
@@ -2,66 +2,40 @@ import pdal
 import tiledb
 import numpy as np
 from math import floor
+import pathlib
 
 from . import Bounds
 
 class Storage(object):
     """ Handles storage of shattered data in a TileDB Database. """
 
-    def __init__(self):
-        self.ctx = tiledb.default_ctx()
-
-        self.schema = None
-        self.tdb = None
-        self.path = None
-
-    def __make_schema(self, atts: list[str], resolution: float, bounds: list[float]) -> tiledb.ArraySchema:
-        """
-        Creates TileDB schema for Storage class.
-
-        Parameters
-        ----------
-        atts : list[str]
-            List of PDAL attributes
-        resolution : float
-            Resolution of a cell
-        bounds : list[float]
-            Bounding box of dataset. [minx, minz(, minz), maxx, maxy(, maxz)]
-
-        Returns
-        -------
-        tiledb.ArraySchema
-            Schema associated with generated TileDB database
-        """
-
-        if len(bounds) == 4:
-            minx, miny, maxx, maxy = bounds
-        elif len(bounds) == 6:
-            minx, miny, minz, maxx, maxy, maxz = bounds
+    def __init__(self, tdb_dir: str, ctx:tiledb.Ctx=None):
+        if not ctx:
+            self.ctx = tiledb.default_ctx()
         else:
-            raise Exception(f"Bounding box must have either 4 or 6 entities. {bounds}")
+            self.ctx = ctx
 
+        #TODO check paths with pathlib
+        self.tdb_dir = tdb_dir
 
-        dims = { d['name']: d['dtype'] for d in pdal.dimensions if d['name'] in atts }
-        xi = floor((maxx - minx) / resolution)
-        yi = floor((maxy - miny) / resolution)
+        #TODO create boths streams at startup
+        self.__open()
+        self.write_flag = False
 
-        dim_row = tiledb.Dim(name="X", domain=(0,xi), dtype=np.float64, ctx=self.ctx)
-        dim_col = tiledb.Dim(name="Y", domain=(0,yi), dtype=np.float64, ctx=self.ctx)
-        domain = tiledb.Domain(dim_row, dim_col, ctx=self.ctx)
+        self.schema = self.tdb_r.schema
 
-        count_att = tiledb.Attr(name="count", dtype=np.int32, ctx=self.ctx)
-        tdb_atts = [tiledb.Attr(name=name, dtype=dims[name], var=True, ctx=self.ctx)
-                    for name in atts]
+    #TODO enter and exit methods to close read and write streams
 
-        schema = tiledb.ArraySchema(ctx=self.ctx, domain=domain, sparse=True,
-            capacity=len(atts) * xi * yi,
-            attrs=[count_att, *tdb_atts], allows_duplicates=True)
-        schema.check()
-        self.schema = schema
-        return schema
+    def __enter__(self):
+        return zip(self.tdb_r, self.tdb_w)
 
-    def create(self, atts:list[str], resolution: float, bounds: list[float], dirname: str):
+    def __exit__(self):
+        self.tdb_r.close()
+        self.tdb_w.close()
+
+    @staticmethod
+    def create(atts:list[str], resolution: float, bounds: list[float],
+               dirname: str, ctx:tiledb.Ctx=None):
         """
         Creates TileDB storage.
 
@@ -75,14 +49,83 @@ class Storage(object):
             Bounding box of dataset. [minx, minz(, minz), maxx, maxy(, maxz)]
         dirname : str
             Path to where TileDB should be created
+        ctx : tiledb.Ctx, optional
+            TileDB Context, by default is None
+
+        Returns
+        -------
+        Storage
+            Returns newly created Storage class
+
+        Raises
+        ------
+        Exception
+            Raises bounding box errors if not of lengths 4 or 6
         """
-        self.path = dirname
-        if tiledb.object_type(dirname) == "array":
-            self.tdb = tiledb.open(dirname, "w")
+
+        #TODO pathlib.path for dirname
+        if not ctx:
+            ctx = tiledb.default_ctx()
+
+        if len(bounds) == 4:
+            minx, miny, maxx, maxy = bounds
+        elif len(bounds) == 6:
+            minx, miny, minz, maxx, maxy, maxz = bounds
         else:
-            schema = self.__make_schema(atts, resolution, bounds)
-            tiledb.SparseArray.create(dirname, schema)
-            self.tdb = tiledb.open(dirname, "w")
+            raise Exception(f"Bounding box must have either 4 or 6 entities. {bounds}")
+
+
+        dims = { d['name']: d['dtype'] for d in pdal.dimensions if d['name'] in atts }
+        xi = floor((maxx - minx) / float(resolution))
+        yi = floor((maxy - miny) / float(resolution))
+
+        dim_row = tiledb.Dim(name="X", domain=(0,xi), dtype=np.float64, ctx=ctx)
+        dim_col = tiledb.Dim(name="Y", domain=(0,yi), dtype=np.float64, ctx=ctx)
+        domain = tiledb.Domain(dim_row, dim_col, ctx=ctx)
+
+        count_att = tiledb.Attr(name="count", dtype=np.int32, ctx=ctx)
+        tdb_atts = [tiledb.Attr(name=name, dtype=dims[name], var=True, ctx=ctx)
+                    for name in atts]
+
+        schema = tiledb.ArraySchema(ctx=ctx, domain=domain, sparse=True,
+            capacity=len(atts) * xi * yi,
+            attrs=[count_att, *tdb_atts], allows_duplicates=True)
+        schema.check()
+
+        tiledb.SparseArray.create(dirname, schema)
+
+        return Storage(dirname, ctx)
+
+    def __open(self):
+        p = pathlib.Path(self.tdb_dir)
+        if tiledb.object_type(self.tdb_dir) == "array":
+            self.tdb_w: tiledb.SparseArray = tiledb.SparseArray(self.tdb_dir, "w", ctx=self.ctx)
+            self.tdb_r: tiledb.SparseArray = tiledb.SparseArray(self.tdb_dir, "r", ctx=self.ctx)
+        elif p.exists():
+            raise Exception(f"""Path {self.tdb_dir} already exists and is not initialized\
+                             for TileDB access.""")
+        else:
+            raise Exception(f"Path {self.tdb_dir} does not exist")
+
+    #TODO what are we reading? queries are probably going to be specific
+    def read(self, xs: np.ndarray, ys: np.ndarray) -> np.ndarray:
+        """
+        Read from the Database
+        Parameters
+        ----------
+        xs : np.ndarray
+            X index
+        ys : np.ndarray
+            Y index
+
+        Returns
+        -------
+        np.ndarray
+            Items found at the indicated cell
+        """
+        if self.write_flag:
+            self.tdb_r.reopen()
+        return self.tdb_r[xs, ys]
 
     def write(self, xs: np.ndarray, ys: np.ndarray, data: np.ndarray) -> None:
         """
@@ -97,6 +140,5 @@ class Storage(object):
         data : np.ndarray
             Numpy object of data values for attributes in each index pairing
         """
-        if not self.tdb:
-            self.tdb = tiledb.open(self.dirname, "w")
-        self.tdb[xs, ys] = data
+        self.tdb_w[xs, ys] = data
+        self.write_flag = True
--- tests/test_storage.py
+++ tests/test_storage.py
@@ -14,10 +14,8 @@ def tdb_filepath(tmp_path_factory) -> str:
     yield os.path.abspath(path)
 
 @pytest.fixture(scope="class")
-def storage(self, tdb_filepath, resolution, attrs, minx, maxx, miny, maxy) -> Storage:
-    s = Storage()
-    s.create(attrs, resolution, [minx, miny, maxx, maxy], tdb_filepath)
-    yield s
+def storage(tdb_filepath, resolution, attrs, minx, maxx, miny, maxy) -> Storage:
+    yield Storage.create(attrs, resolution, [minx, miny, maxx, maxy], tdb_filepath)
 
 class Test_Storage(object):
 
@@ -33,7 +31,7 @@ class Test_Storage(object):
 
 
     def test_local(self, storage: Storage, attrs: list[str], dims):
-        s = tiledb.ArraySchema.load(storage.path)
+        s = tiledb.ArraySchema.load(storage.tdb_dir)
 
         assert s.has_attr('count')
         assert s.attr('count').dtype == np.int32
@@ -42,7 +40,7 @@ class Test_Storage(object):
             assert s.has_attr(a)
             assert s.attr(a).dtype == dims[a]
 
-@pytest.skip(reason="Not finishes")
-class Test_Initialize(object):
-    def test_command(self, cli_runner):
-        res = cli_runner.invoke(initialize, [])
+# class Test_Initialize(object):
+#     @pytest.skip(reason="Not finishes")
+#     def test_command(self, cli_runner):
+#         res = cli_runner.invoke(initialize, [])
