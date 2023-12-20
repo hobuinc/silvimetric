aa38936e22c1b78e643f8980bc00a28161550f56
Howard Butler <hobu.inc@gmail.com>
2023-12-20 14:41:54 -0600

    cleanup Tile

--- src/silvimetric/resources/tile.py
+++ src/silvimetric/resources/tile.py
@@ -8,7 +8,7 @@ import math
 
 class Tile:
 
-    def __init__(self, data: Data, storage: Storage, size: int, origin: list[int]):
+    def __init__(self, data: Data, size: int, origin: list[int], storage: Storage, ):
         self.data = data
         self.storage = storage
         self.size = size
@@ -17,30 +17,33 @@ class Tile:
         self.x1 = origin[0] * size
         self.y1 = origin[1] * size
 
-        # self.x1 = math.floor((minx - self.root.minx) / resolution)
-        # self.y1 = math.floor((self.root.maxy - maxy) / resolution)
-        # self.x2 = math.floor((maxx - self.root.minx) / resolution)
-        # self.y2 = math.floor((self.root.maxy - miny) / resolution)
-
-    def get_count(self):
-
-        resolution = self.storage.root.resolution
-        bminx = self.storage.root.minx
-        bminy = self.storage.root.miny
-        bmaxx = self.storage.root.maxx
-        bmaxy = self.storage.root.maxy
-
-        x_buf = 1 if bmaxx % resolution != 0 else 0
-        y_buf = 1 if bmaxy % resolution != 0 else 0
-
-        # make bounds in scale with the desired resolution
-        minx = bminx + (self.x0 * self.resolution)
-        maxx = bminx + ((self.x2 + x_buf) * self.resolution)
-        miny = bmaxy - ((self.y2 + y_buf) * self.resolution)
-        maxy = bmaxy - (self.y1 * self.resolution)
-
+    def get_bounds(self):
         resolution = self.storage.root.resolution
         minx = self.storage.root.minx + (self.x0 * resolution)
-        miny = self.storage.root.minx + (self.y0 * resolution)
+        miny = self.storage.root.miny + (self.y0 * resolution)
+        maxx = self.storage.root.minx + (self.x1 * resolution)
+        maxy = self.storage.root.miny + (self.y1 * resolution)
         b = Bounds(minx, miny, maxx, maxy)
+        return b
+    bounds = property(get_bounds)
+
+    def get_count_estimate(self):
+        b = self.get_bounds()
+        return self.data.estimate_count(b)
+    count_estimate = property(get_count_estimate)
+
+    def get_count(self):
+        b = self.get_bounds()
+        return self.data.count(b)
+    count = property(get_count)
+
+    def bisect(self):
+        centerx = self.minx + ((self.maxx - self.minx)/ 2)
+        centery = self.miny + ((self.maxy - self.miny)/ 2)
+        yield from [
+            Bounds(self.minx, self.miny, centerx, centery), # lower left
+            Bounds(centerx, self.miny, self.maxx, centery), # lower right
+            Bounds(self.minx, centery, centerx, self.maxy), # top left
+            Bounds(centerx, centery, self.maxx, self.maxy), # top right
+        ]
 
