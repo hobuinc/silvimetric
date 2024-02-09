6c0a362f2dc8286f47f2e477566a98709694b47f
bmcgaughey1 <bmcgaughey1@gmail.com>
2024-02-09 15:22:20 -0500

    comments

--- src/silvimetric/resources/metric.py
+++ src/silvimetric/resources/metric.py
@@ -126,6 +126,7 @@ def m_madmode(data):
 
 # TODO test various methods for interpolation=... I think the default
 # matches FUSION method
+# not sure how an array of metrics will be ingested by shatter
 def m_percentiles(data):
     return(np.percentile(data, [1,5,10,20,25,30,40,50,60,70,75,80,90,95,99]))
 
