ba3a69bb633e6e9b96258b82a1ccf9f5dba002ac
bmcgaughey1 <bmcgaughey1@gmail.com>
2024-03-07 09:41:33 -0500

    m_mode again

--- src/silvimetric/resources/metric.py
+++ src/silvimetric/resources/metric.py
@@ -94,7 +94,7 @@ def m_mode(data, htthreshold, coverthreshold):
     
     bins = np.histogram(d, bins = nbins, density = False)
 
-    thebin = np.argmax(bins)
+    thebin = np.argmax(bins, keepdims=False)
     
     # compute the height and return...nbins - 1 is to get the bottom Z of the bin
     return minv + thebin * (maxv - minv) / (nbins - 1)
