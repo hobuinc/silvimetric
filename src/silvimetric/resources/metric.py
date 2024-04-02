dd6053e19920a1acd9fbbc20121041169bcf2a12
bmcgaughey1 <bmcgaughey1@gmail.com>
2024-04-02 12:34:33 -0400

    Added docstrings to all metrics functions

--- src/silvimetric/resources/metric.py
+++ src/silvimetric/resources/metric.py
@@ -134,30 +134,151 @@ def m_mode(data):
     return minv + thebin * (maxv - minv) / (nbins - 1)
 
 def m_median(data):
+    """
+    Return the median.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return np.median(data)
 
 def m_min(data):
+    """
+    Return the minimum value.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return np.min(data)
 
 def m_max(data):
+    """
+    Return the maximum value.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return np.max(data)
 
 def m_stddev(data):
+    """
+    Return the standard deviation of values.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return np.std(data)
 
 # start of new metrics to match FUSION
 def m_variance(data):
+    """
+    Return the variance of values.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return np.var(data)
 
 def m_cv(data):
+    """
+    Return the coefficient of variation of values.
+    Standard deviation / mean.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return np.std(data) / np.mean(data)
 
 # TODO check performance of other methods
 def m_abovemean(data):
+    """
+    Return the proportion of values above the mean.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return (data > np.mean(data)).sum() / len(data)
 
 # TODO check performance of other methods
 def m_abovemode(data):
+    """
+    Return the proportion of values above the mode.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return (data > m_mode(data)).sum() / len(data)
 
 # both stats.skew and stats.kurtosis throw a warning:
@@ -168,6 +289,21 @@ def m_abovemode(data):
 # GridMetrics computes these using a different formula that may also have trouble
 # with numeric stability.
 def m_skewness(data):
+    """
+    Return the skewness of values.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     if len(data) < 4:
         return NODATA
 
@@ -179,6 +315,21 @@ def m_skewness(data):
     return s
 
 def m_kurtosis(data):
+    """
+    Return the kurtosis of values.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     if len(data) < 4:
         return NODATA
 
@@ -190,34 +341,155 @@ def m_kurtosis(data):
     return k
 
 def m_aad(data):
+    """
+    Return the average absolute deviation from the mean.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     m = m_mean(data)
     return np.mean(np.absolute(data - m))
 
 def m_madmedian(data):
+    """
+    Return the median absolute difference from the median value.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return stats.median_abs_deviation(data)
 
 def m_madmean(data):
+    """
+    Return the median absolute difference from the mean value.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return stats.median_abs_deviation(data, center=np.mean)
 
 # TODO needs work
 def m_madmode(data):
+    """
+    Return the median absolute difference from the mode value.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     m = m_mode(data)
     return np.median(np.absolute(data - m))
 
 # TODO test various methods for interpolation=... for all percentile-related metrics
 # I think the default matches FUSION method but need to test
 def m_iq(data):
+    """
+    Return the interquartile difference.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return stats.iqr(data)
 
 def m_90m10(data):
+    """
+    Return the 90th percentile minus the 10th percentile.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     p = np.percentile(data, [10,90])
     return p[1] - p[0]
 
 def m_95m05(data):
+    """
+    Return the 95th percentile minnus the 5th percentile.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     p = np.percentile(data, [5,95])
     return p[1] - p[0]
 
 def m_crr(data):
+    """
+    Return the cannopy relief ratio.
+    (mean - min) / (max - min).
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     maxv = np.max(data)
     minv = np.min(data)
     if minv == maxv:
@@ -226,9 +498,39 @@ def m_crr(data):
     return (np.mean(data) - minv) / (maxv - minv)
 
 def m_sqmean(data):
+    """
+    Return the square root of the average squared value.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return np.sqrt(np.mean(np.square(data)))
 
 def m_cumean(data):
+    """
+    Return the cube root of the average cubed value.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return np.cbrt(np.mean(np.power(np.absolute(data), 3)))
 
 # TODO compute L-moments. These are done separately because we only add
@@ -243,12 +545,42 @@ def m_cumean(data):
 
 # L1 is same as mean...compute using np.mean for speed
 def m_l1(data):
+    """
+    Return the first L-moment (mean).
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     if len(data) < 4:
         return NODATA
 
     return np.mean(data)
 
 def m_l2(data):
+    """
+    Return the second L-moment.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     if len(data) < 4:
         return NODATA
 
@@ -256,6 +588,21 @@ def m_l2(data):
     return l[1]
 
 def m_l3(data):
+    """
+    Return the third L-moment.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     if len(data) < 4:
         return NODATA
 
@@ -263,6 +610,21 @@ def m_l3(data):
     return l[2]
 
 def m_l4(data):
+    """
+    Return the fourth L-moment.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     if len(data) < 4:
         return NODATA
 
@@ -270,6 +632,21 @@ def m_l4(data):
     return l[3]
 
 def m_lcv(data):
+    """
+    Return the L-moment coefficient of variation (L2 / mean).
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     if len(data) < 4:
         return NODATA
 
@@ -281,6 +658,21 @@ def m_lcv(data):
     return l[1] / l[0]
 
 def m_lskewness(data):
+    """
+    Return the L-moment skewness (L3 / L2).
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     if len(data) < 4:
         return NODATA
 
@@ -291,6 +683,21 @@ def m_lskewness(data):
     return l[2] / l[1]
 
 def m_lkurtosis(data):
+    """
+    Return the L-moment kurtosis (L4 / L2).
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     if len(data) < 4:
         return NODATA
 
@@ -306,51 +713,308 @@ def m_lkurtosis(data):
 #    return(np.percentile(data, [1,5,10,20,25,30,40,50,60,70,75,80,90,95,99]))
 
 def m_p01(data):
+    """
+    Return the 1st percentile value.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return(np.percentile(data, 1))
 
 def m_p05(data):
+    """
+    Return the 5th percentile value.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return(np.percentile(data, 5))
 
 def m_p10(data):
+    """
+    Return the 10th percentile value.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return(np.percentile(data, 10))
 
 def m_p20(data):
+    """
+    Return the 20th percentile value.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return(np.percentile(data, 20))
 
 def m_p25(data):
+    """
+    Return the 25th percentile value.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return(np.percentile(data, 25))
 
 def m_p30(data):
+    """
+    Return the 30th percentile value.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return(np.percentile(data, 30))
 
 def m_p40(data):
+    """
+    Return the 40th percentile value.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return(np.percentile(data, 40))
 
 def m_p50(data):
+    """
+    Return the 50th percentile value.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return(np.percentile(data, 50))
 
 def m_p60(data):
+    """
+    Return the 60th percentile value.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return(np.percentile(data, 60))
 
 def m_p70(data):
+    """
+    Return the 70th percentile value.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return(np.percentile(data, 70))
 
 def m_p75(data):
+    """
+    Return the 75th percentile value.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return(np.percentile(data, 75))
 
 def m_p80(data):
+    """
+    Return the 80th percentile value.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return(np.percentile(data, 80))
 
 def m_p90(data):
+    """
+    Return the 90th percentile value.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return(np.percentile(data, 90))
 
 def m_p95(data):
+    """
+    Return the 95th percentile value.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return(np.percentile(data, 95))
 
 def m_p99(data):
+    """
+    Return the 99th percentile value.
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     return(np.percentile(data, 99))
 
 def m_profilearea(data):
+    """
+    Return the profile area.
+
+    Profile area was first described in 
+
+        Hu, Tianyu, Qin Ma, Yanjun Su, John J. Battles, Brandon M. Collins, 
+        Scott L. Stephens, Maggi Kelly, Qinghua Guo. 2019. A simple and 
+        integrated approach for fire severity assessment using bi-temporal 
+        airborne LiDAR data. Int J. Appl Earth Obs Geoinformation, 78 (2019): 25-38.
+
+    as the area under the height percentile profile or curve. They found the 
+    metric useful to compare pre- and post-fire canopy structure at both the 
+    individual tree and pixel scales. The implementation in CloudMetrics and 
+    GridMetrics varies from that described in Hu et al. 2019. Heights are 
+    normalized using the 99th percentile height instead of the maximum height 
+    to eliminate problems related to high outliers and the area under the 
+    percentile curve is computed directly from 1 percent slices instead of 
+    fitting a polynomial to the percentile heights and computing the area 
+    under the polynomial
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        Data for dimension
+
+    Returns
+    -------
+    float
+        Value for metric computed using values in data. Returns
+        NODATA value when metric cannot be computed
+    """
+
     # sanity check...must have valid heights/elevations
     if np.max(data) <= 0:
         return NODATA
