86b7662ec185a96e2ca9a30299c4f8e890658069
kylemann16 <kyle@hobu.co>
2024-04-23 15:22:16 -0500

    fix imports for overarching package

--- src/silvimetric/__init__.py
+++ src/silvimetric/__init__.py
@@ -1,1 +1,17 @@
-__version__ = '1.0.0'
+__version__ = '1.1.2'
+
+from .resources.bounds import Bounds
+from .resources.extents import Extents
+from .resources.storage import Storage
+from .resources.metric import Metric, Metrics
+from .resources.log import Log
+from .resources.data import Data
+from .resources.entry import Attribute, Pdal_Attributes, Attributes
+from .resources.config import StorageConfig, ShatterConfig, ExtractConfig, ApplicationConfig
+
+from .commands.shatter import shatter
+from .commands.extract import extract
+from .commands.info import info
+from .commands.scan import scan
+from .commands.initialize import initialize
+from .commands.manage import delete, resume, restart
