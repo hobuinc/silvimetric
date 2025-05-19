fixed 1st_cover metric and variance

src/silvimetric/resources/metrics/counts.py
@@ -88,7 +88,7 @@ first_count_above_mode = Metric(
 )
 """number of first returns"""
 first_count = Metric(
-    '1st_count_above',
+    '1st_count',
     np.int32,
     count_fn,
     filters=[first_returns_filter],

src/silvimetric/resources/metrics/covers.py
@@ -19,6 +19,9 @@ def cover_fn(data, *args):
     count = args[0]
     return data.count() / count * 100
 
+def cover_fn2(data, *args):
+    count = args[0]
+    return data.count() / count * 100
 
 def cover_above_val(data, *args):
     count = args[0]
@@ -54,7 +57,7 @@ all_cover_above_mode = Metric(
 first_cover = Metric(
     '1st_cover_above_htbreak',
     np.float32,
-    cover_fn,
+    cover_fn2,
     dependencies=[first_count],
     filters=[first_returns_filter],
     attributes=[A['ReturnNumber']],

src/silvimetric/resources/metrics/p_moments.py
@@ -27,6 +27,7 @@ def m_mean(data, *args):
 
 
 def m_variance(data, *args):
+    return ((data - data.mean())**2).sum() / (data.count() - 1)
     return args[0][0]
 
 
@@ -36,11 +37,12 @@ def m_skewness(data, *args):
         # * (Points[l].Value - GridMean)
         # * (Points[l].Value - GridMean);
     # TODO maybe htis?
-    #((data - data.mean())**3).sum() / ((data.count() - 1) * np.std(data)**3)
-    return args[0][1]
+    return ((data - data.mean())**3).sum() / ((data.count() - 1) * np.std(data)**3)
+    # return args[0][1]
 
 
 def m_kurtosis(data, *args):
+    return ((data - data.mean())**4).sum() / ((data.count() - 1) * np.std(data)**4)
     return args[0][2]
 
 
@@ -52,7 +54,7 @@ variance = Metric(
     name='variance',
     dtype=np.float32,
     method=m_variance,
-    dependencies=[moment_base],
+    dependencies=[],
 )
 skewness = Metric(
     name='skewness',