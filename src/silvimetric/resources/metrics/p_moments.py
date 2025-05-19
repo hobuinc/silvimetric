fixing profile area calculation

src/silvimetric/resources/metrics/p_moments.py
@@ -1,24 +1,6 @@
 import numpy as np
-from scipy.stats import moment
-import warnings
 
 from ..metric import Metric
-
-
-def m_moments(data, *args):
-    mean = args[0]
-    with warnings.catch_warnings():
-        warnings.filterwarnings('ignore', category=RuntimeWarning)
-        m = moment(
-            data, center=mean, order=[2, 3, 4], nan_policy='omit'
-        ).tolist()
-    # if any(mean):
-    # else:
-    #     with warnings.catch_warnings():
-    #         warnings.filterwarnings('ignore', category=RuntimeWarning)
-    #         m = moment(data, order=[2,3,4], nan_policy='propagate').tolist()
-
-    return m
 
 
 def m_mean(data, *args):
@@ -27,47 +9,28 @@ def m_mean(data, *args):
 
 
 def m_variance(data, *args):
-    return ((data - data.mean())**2).sum() / (data.count() - 1)
-    return args[0][0]
+    # copy FUSION's variance approach
+    return ((data - data.mean()) ** 2).sum() / (data.count() - 1)
 
 
 def m_skewness(data, *args):
-    # GridSkewness +=
-        # (Points[l].Value - GridMean)
-        # * (Points[l].Value - GridMean)
-        # * (Points[l].Value - GridMean);
-    # TODO maybe htis?
-    return ((data - data.mean())**3).sum() / ((data.count() - 1) * np.std(data)**3)
-    # return args[0][1]
+    # copy FUSION's approximation of skewness
+    return ((data - data.mean()) ** 3).sum() / (
+        (data.count() - 1) * np.std(data) ** 3
+    )
 
 
 def m_kurtosis(data, *args):
-    return ((data - data.mean())**4).sum() / ((data.count() - 1) * np.std(data)**4)
-    return args[0][2]
+    # copy FUSION's approximation of kurtosis
+    return ((data - data.mean()) ** 4).sum() / (
+        (data.count() - 1) * np.std(data) ** 4
+    )
 
 
 mean = Metric(name='mean', dtype=np.float32, method=m_mean)
-moment_base = Metric(
-    name='moment_base', dtype=object, method=m_moments, dependencies=[mean]
-)
-variance = Metric(
-    name='variance',
-    dtype=np.float32,
-    method=m_variance,
-    dependencies=[],
-)
-skewness = Metric(
-    name='skewness',
-    dtype=np.float32,
-    method=m_skewness,
-    dependencies=[moment_base],
-)
-kurtosis = Metric(
-    name='kurtosis',
-    dtype=np.float32,
-    method=m_kurtosis,
-    dependencies=[moment_base],
-)
+variance = Metric(name='variance', dtype=np.float32, method=m_variance)
+skewness = Metric(name='skewness', dtype=np.float32, method=m_skewness)
+kurtosis = Metric(name='kurtosis', dtype=np.float32, method=m_kurtosis)
 
 product_moments: dict[str, Metric] = dict(
     mean=mean, variance=variance, skewness=skewness, kurtosis=kurtosis

src/silvimetric/resources/metrics/percentiles.py
@@ -90,7 +90,7 @@ def m_profile_area(data, *args):
         # compute area under normalized percentile height curve using composite
         # trapeziod rule
         grid_pa = p0 / p99
-        pcts = np.array(p[1:98])
+        pcts = np.array(p[1:99])
         areas = pcts * 2 / p99
         pa = grid_pa + areas.sum() + 1
 